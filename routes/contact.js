fcc3d3f0864ea6216a7890188453732dd9f00f16
FEBIN EMMANUEL <febincanada@gmail.com>
2021-11-23 11:12:39 -0500

    updated all variables

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('contact');
+    res.render('contact',{bond7:'You can count on me at all the times. I am also an experienced firearms specialiat. I own all the fastest Aston Martins out there.'});
 });
 
 module.exports = router
