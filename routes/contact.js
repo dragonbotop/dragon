edb8e3864a0b782e8711c2d7f54bfac9d2dd5c76
FEBIN EMMANUEL <febincanada@gmail.com>
2021-11-23 10:08:42 -0500

    project  and about page updated

--- routes/contact.js
+++ routes/contact.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('Projects/index',{
-       recentworks:'These are some of my recent works ',
-       
-
-    });
+    res.render('contact');
 });
 
 module.exports = router
